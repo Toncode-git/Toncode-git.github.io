7f0144220f916f70cc65b072cdcb8022135855e5
Toncode-git <alejandroantonioramirez@gmail.com>
2026-01-04 15:50:39 -0500

    folder workflow organization

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("hello world")
+
+// this is a comment
+
+console.log("my name is " + "alex")
+
+
+// this is a text to round numbers 
+
+let numbers = 0.9;
+console.log(Math.round(numbers));
